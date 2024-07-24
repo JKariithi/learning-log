72e555cddb9ba117e36c762022665a283ac8aeab
JKariithi <kariithijanice@gmail.com>
2024-07-24 10:10:13 +0300

    Defining models -We have defined Topic model that displays the title the learner is learning about plus the exact sate and time

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Tapic(models.Model):
+    """Stores the title of the topic the user is learning about """
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_add_now=True)
+     
+    def __str__(self):
+        """Return a string representation of the model"""
+        return self.text
+
+
+
+
